ee47a5ce091c5e7eff09bf9752c950f86b59fd40
Defensorem <Defensorem@outlook.com>
2024-05-17 10:32:31 +0300

    Healing station added to the tutorial, zone triggers added

--- Assets/Scripts/Other/TutorialScene.cs
+++ Assets/Scripts/Other/TutorialScene.cs
@@ -23,15 +23,19 @@ public class TutorialScene : MonoBehaviour
     [SerializeField] private GameObject pointingToPlatform;
 
     [Header("Healing station")]
-    [SerializeField] private GameObject healingStation;
+    [SerializeField] private HealingStation healingStation;
+    [SerializeField] private GameObject pointingToHealingStation;
+    [SerializeField] private GameObject healingStationTrigger;
 
     [Header("Audio")]
     [SerializeField] private AudioClip[] tutorialAudioClipsN1;
     [SerializeField] private AudioClip[] tutorialAudioClipsN2;
     [SerializeField] private AudioClip[] tutorialAudioClipsN3;
     [SerializeField] private AudioClip[] tutorialAudioClipsN4;
+    [SerializeField] private AudioClip[] tutorialAudioClipsN5;
+    [SerializeField] private AudioClip[] tutorialAudioClipsN6;
 
-    [Header("TutorialEnded")]
+    [Header("OnTutorialEnd")]
     [SerializeField] private GameObject roomButton;
     [SerializeField] private GameObject batya;
     [SerializeField] private PlayerData playerData;
@@ -43,6 +47,9 @@ public class TutorialScene : MonoBehaviour
     public bool gunPicked = false;
     public bool targetsSetupped = false;
     public bool pointingToPlatformActive = false;
+    public bool pointingToHealingStationActive = false;
+    public bool waitingToUseHealingStation = false;
+    public bool tutorialEnded = false;
 
     private AudioSource audioSource;
 
@@ -50,25 +57,46 @@ public class TutorialScene : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
         weaponToTake.SetActive(false);
+        healingStationTrigger.SetActive(false);
         voiceAssistant.gameObject.SetActive(false);
+        healingStation.gameObject.SetActive(false);
     }
 
     private void Update()
     {
-        if (weaponToTake == null && tutorialStarted && !gunPicked)
+        if (weaponToTake == null && tutorialStarted && !gunPicked && !tutorialEnded)
         {
             gunPicked = true;
             pointingToWeapon.SetActive(false);
             Debug.Log("Speech2");
             StartCoroutine(PlayTutorialAudioClips(tutorialAudioClipsN2, ActivatePointingToPlatform));
         }
-        if (targetsSetupped)
+        if (targetsSetupped && !tutorialEnded)
         {
             if (AllTargetsInactive())
             {
                 Debug.Log("Speech4");
                 targetsSetupped = false;
-                StartCoroutine(PlayTutorialAudioClips(tutorialAudioClipsN4, TutorialEnded));
+                StartCoroutine(PlayTutorialAudioClips(tutorialAudioClipsN4, ActivatePointingToHealingStation));
+            }
+        }
+        if (pointingToHealingStationActive && !tutorialEnded)
+        {
+            if (healingStationTrigger == null)
+            {
+                pointingToHealingStation.SetActive(false);
+                pointingToHealingStationActive = false;
+                Debug.Log("Speech5");
+                StartCoroutine(PlayTutorialAudioClips(tutorialAudioClipsN5, ActivateWaitingToUseHealingStation));
+            }
+        }
+
+        if (waitingToUseHealingStation && !tutorialEnded)
+        {
+            if (healingStation.restorationsAmount == 0)
+            {
+                Debug.Log("Speech6");
+                StartCoroutine(PlayTutorialAudioClips(tutorialAudioClipsN6, OnTutorialEnd));
             }
         }
 
@@ -80,7 +108,10 @@ public class TutorialScene : MonoBehaviour
         Debug.Log("Training1");
         TV.GetComponent<Renderer>().material = TVMaterial;
         Debug.Log("Speech1");
-        StartCoroutine(PlayTutorialAudioClips(tutorialAudioClipsN1, ActivatePointingToWeapon));
+        if (!tutorialEnded)
+        {
+            StartCoroutine(PlayTutorialAudioClips(tutorialAudioClipsN1, ActivatePointingToWeapon));
+        }
     }
 
     IEnumerator PlayTutorialAudioClips(AudioClip[] clips, Action callback)
@@ -100,7 +131,10 @@ public class TutorialScene : MonoBehaviour
         Debug.Log("Training2");
         pointingToPlatform.SetActive(false);
         Debug.Log("Speech3");
-        StartCoroutine(PlayTutorialAudioClips(tutorialAudioClipsN3, SetupTargets));
+        if (!tutorialEnded)
+        {
+            StartCoroutine(PlayTutorialAudioClips(tutorialAudioClipsN3, SetupTargets));
+        }
     }
 
     public void SetupTargets()
@@ -124,6 +158,15 @@ public class TutorialScene : MonoBehaviour
         platform.GetComponent<Renderer>().material = platformHighlight;
         pointingToPlatformActive = true;
     }
+
+    private void ActivatePointingToHealingStation()
+    {
+        pointingToHealingStation.SetActive(true);
+        healingStationTrigger.SetActive(true);
+        healingStation.gameObject.SetActive(true);
+        pointingToHealingStationActive = true;
+    }
+
     private bool AllTargetsInactive()
     {
         foreach (GameObject target in targetsList)
@@ -136,8 +179,14 @@ public class TutorialScene : MonoBehaviour
         return true;
     }
 
-    private void TutorialEnded()
+    private void ActivateWaitingToUseHealingStation()
     {
+        waitingToUseHealingStation = true;
+    }
+
+    private void OnTutorialEnd()
+    {
+        tutorialEnded = true;
         roomButton.SetActive(true);
         batya.SetActive(true);
         voiceAssistant.gameObject.SetActive(true);
